0d74b84a1813a6e1c00b1d0660bbf95ddf84ae2b
7Araby7 <gabrielbrunhara2002@gmail.com>
2024-11-27 14:10:03 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './sapone.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Nunca te pedi nada
+        </p>
+        <a
+          className="App-link"
+          href="https://www.tiktok.com/@_clip_n_chill"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vem ver o melhor tiktok de todos
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
